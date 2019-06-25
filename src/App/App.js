556c65561d759417c107090b6503513d2b3024c9
Wayne Chipchase <wayne.chipchase@gmail.com>
2019-06-24 18:25:00 -0600

    setup finished

---
+++ src/App/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          <button className='btn btn-danger'>Fish Store</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
