bbe37f3e3479b54949b7ccc83ae333d67538750f
Wayne Chipchase <wayne.chipchase@gmail.com>
2019-06-24 20:28:14 -0600

    added login

--- src/App/App.js
+++ src/App/App.js
@@ -1,14 +1,30 @@
 import React from 'react';
+import firebase from 'firebase/app';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Auth from '../components/Auth/Auth';
 import Home from '../components/Home/Home';
 import fbConnection from '../helpers/data/connections';
 
+
 fbConnection();
 class App extends React.Component {
   state = {
     authed: false,
+  }
+
+  componentDidMount() {
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ authed: true });
+      } else {
+        this.setState({ authed: false });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.removeListener();
   }
 
   render() {
