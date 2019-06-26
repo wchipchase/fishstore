42abfccdf32b5e1715c6b051d876e3972aac5879
Wayne Chipchase <wayne.chipchase@gmail.com>
2019-06-25 20:51:38 -0600

    picture printing

--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -1,11 +1,30 @@
 import React from 'react';
+import fishData from '../../helpers/data/fishData';
+import Fish from '../Fish/Fish';
 import './Inventory.scss';
 
 class Inventory extends React.Component {
+  state = {
+    fishes: [],
+  }
+
+  componentDidMount() {
+    fishData.getFishes()
+      .then(fishes => this.setState({ fishes }))
+      .catch(err => console.error('Could not get fishes', err));
+  }
+
   render() {
+    const fishComponents = this.state.fishes.map(fish => (
+      <Fish key={fish.id} fish={fish} />
+    ));
+
     return (
       <div className="Inventory">
-        <h1>Inventory</h1>
+        <h2>Inventory</h2>
+        <ul className='fishes'>
+          { fishComponents }
+        </ul>
   </div>
     );
   }
